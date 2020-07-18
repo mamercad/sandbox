a3255fe2e341f4a2cd6c2be7203cc2eb7b808f74
Mark Mercado <mamercad@gmail.com>
2020-07-18 10:51:31 -0400

    testing

--- client.py
+++ client.py
@@ -11,48 +11,56 @@ import sys
 import signal
 from requests.exceptions import HTTPError
 
+reqs_per_sec = 3
+endpoint     = None
+port         = None
+attempts     = 0
+successes    = 0
+failures     = 0
+
 try:
     endpoint = subprocess.check_output('kubectl -n sandbox get svc hello -o jsonpath="{.status.loadBalancer.ingress[*].ip}"', shell=True, universal_newlines=True)
-    port = subprocess.check_output('kubectl -n sandbox get svc hello -o jsonpath="{.spec.ports[*].port}"', shell=True, universal_newlines=True)
+    port     = subprocess.check_output('kubectl -n sandbox get svc hello -o jsonpath="{.spec.ports[*].port}"', shell=True, universal_newlines=True)
 except Exception as e:
-    print(e)
+    print(f'Exception: {e}')
     sys.exit(1)
 
 def signal_handler(signal, frame):
     global attempts, successes, failures
+    successe_rate = 100.0 * successes / attempts
+    failure_rate  = 100.0 * failures  / attempts
     print()
-    print('attempts: {}'.format(attempts))
-    print('successes: {} ({}%)'.format(successes, 100.0*successes/attempts))
-    print('failures: {} ({}%)'.format(failures, 100.0*failures/attempts))
+    print('-'*120)
+    print(f'attempts:  {attempts}')
+    print(f'successes: {successes} ({successe_rate}%)')
+    print(f'failures:  {failures} ({failure_rate}%)')
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
 
-attempts  = 0
-successes = 0
-failures  = 0
-
 while True:
 
-    attempts += 1
-    status_code = 0
+    attempts    += 1
+    status_code =  0
 
     try:
 
         d = datetime.now(pytz.timezone("UTC"))
         j = {
-            'year': d.year,
-            'month': d.month,
-            'day': d.day,
-            'hour': d.hour,
-            'minute': d.minute,
-            'second': d.second,
+            'year'       : d.year,
+            'month'      : d.month,
+            'day'        : d.day,
+            'hour'       : d.hour,
+            'minute'     : d.minute,
+            'second'     : d.second,
             'microsecond': d.microsecond,
         }
-        req_start = time.time()
-        r = requests.get('http://{}:{}'.format(endpoint, port), timeout=1)
-        req_finish = time.time()
+
+        req_start   = time.time()
+        r           = requests.get('http://{}:{}'.format(endpoint, port), timeout=1)
+        req_finish  = time.time()
         status_code = r.status_code
+
         r.raise_for_status()
 
         # print("attempt: {}\n  local:  {}\n  remote: {}\n  tag:    {}\n".format(attempts, j, r.json()['date'], r.json()['tag']))
@@ -60,24 +68,23 @@ while True:
 
         remote_date = r.json()['date']
         remote_tag  = r.json()['tag']
+        ttlb        = req_finish - req_start
 
-        print("attempt: {} ({})".format(attempts, remote_tag))
-        print("  local date:  {}".format(j))
-        print("  remote date: {}".format(remote_date))
-        # print("  tag:         {}".format(remote_tag))
-        # print("  ttfb: {}".format(r.elapsed))
-        print("  ttlb:        {}".format(req_finish - req_start))
+        print('-'*120)
+        print(f'attempt: {attempts} (HTTP {status_code}) (TAG {remote_tag})')
+        print(f'  local date:  {j}')
+        print(f'  remote date: {remote_date}')
+        print(f'  ttlb:        {ttlb}')
 
         successes += 1
 
-    except HTTPError as http_e:
-
-        print("{} : {} : {} : {}".format(attempts, j, status_code, http_e))
-        failures += 1
-
     except Exception as e:
 
-        print("{} : {} : {} : {}".format(attempts, j, status_code, e))
+        print('-'*120)
+        print(f'attempt: {attempts} (HTTP {status_code})')
+        print(f'  local date: {j}')
+        print(f'  exception:  {e}')
+
         failures += 1
 
-    time.sleep(0.5)
+    time.sleep( 1 / reqs_per_sec )
