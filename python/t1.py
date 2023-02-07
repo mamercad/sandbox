b87eab5b9226760e3c3cc78e999bd1562ddd85c4
Mark Mercado <mamercad@gmail.com>
2023-02-07 05:38:24 -0500

    Iterating

--- python/t1.py
+++ python/t1.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
 class A:
-    def __init__(self):
+    def __init__(self: object) -> None:
         self.data = dict(a="a", b="b", c="c")
-    def get(self):
+    def get(self: object) -> dict:
         return self.data
 
 class B:
-    def __init__(self):
+    def __init__(self: object) -> None:
         self.data = dict(a="a", b="b", c="c")
-    def get(self):
+    def get(self: object) -> dict:
         return self.data
 
 class C:
-    def __init__(self):
+    def __init__(self: object) -> None:
         self.a = A()
         self.b = B()
 
-    def get(self):
+    def get(self: object) -> bool:
         if self.a.get().get("a") == "a":
             return True
         else:
--- python/test_t1.py
+++ python/test_t1.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-# import pytest
 from unittest.mock import patch
 from t1 import C
-# from t1 import A, B, C
 
 def test1():
     c = C()
@@ -11,5 +9,10 @@ def test1():
 @patch("t1.A.get")
 def test2(aget):
     aget.get.return_value = dict(a="b", b="c", c="a")
+    c = C()
+    assert not c.get()
+
+@patch("t1.A.get", return_value=dict(a="b", b="c", c="a"), autospec=True)
+def test3(aget):
     c = C()
     assert not c.get()
