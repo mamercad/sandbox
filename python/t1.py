698e991c40285449e1903d1a389ca2cf7665ab5f
Mark Mercado <mamercad@gmail.com>
2023-02-07 05:42:19 -0500

    Add and run black

--- python/t1.py
+++ python/t1.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
+
 class A:
     def __init__(self: object) -> None:
         self.data = dict(a="a", b="b", c="c")
+
     def get(self: object) -> dict:
         return self.data
+
 
 class B:
     def __init__(self: object) -> None:
         self.data = dict(a="a", b="b", c="c")
+
     def get(self: object) -> dict:
         return self.data
+
 
 class C:
     def __init__(self: object) -> None:
--- python/test_t1.py
+++ python/test_t1.py
@@ -2,15 +2,18 @@
 from unittest.mock import patch
 from t1 import C
 
+
 def test1():
     c = C()
     assert c.get()
+
 
 @patch("t1.A.get")
 def test2(aget):
     aget.get.return_value = dict(a="b", b="c", c="a")
     c = C()
     assert not c.get()
+
 
 @patch("t1.A.get", return_value=dict(a="b", b="c", c="a"), autospec=True)
 def test3(aget):
